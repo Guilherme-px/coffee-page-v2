b428c8940f96b568e61bb0aeb07833d14e224e9a
Guilherme-px <guilherme.s.goncalves@outlook.com>
2026-09-02 17:09:28 -0300

    test: stub ResizeObserver in nuxt environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
                             },
                         },
                     },
+                    setupFiles: ["test/nuxt/setup.ts"],
                 },
             }),
         ],
